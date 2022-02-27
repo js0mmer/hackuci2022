8a3ec1ce87a6932da5dc1a9b81a7a9f7a9b1d396
Jacob Sommer <jacob.sommer@comcast.net>
2022-02-27 00:44:17 -0800

    Fix whitespace

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 class Home extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+  // constructor(props) {
+  //     super(props);
+  // }
 
-    componentDidMount() {
-    }
+  componentDidMount() {
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>hello world</h1>
-                <a href="http://localhost:8080/login">Login</a>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <a href="http://localhost:8080/login">Login</a>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -2,32 +2,32 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 function Upload() {
-    let [searchParams] = useSearchParams();
-    let state = decodeURIComponent(searchParams.get("state"));
+  let [searchParams] = useSearchParams();
+  let state = decodeURIComponent(searchParams.get("state"));
 
-    const handleUpload = (event) => {
-        if (event.target.files && event.target.files[0]) {
-            const img = event.target.files[0];
-            const form = new FormData();
-            form.append('image', img);
-            form.append('state', encodeURIComponent(state))
+  const handleUpload = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const img = event.target.files[0];
+      const form = new FormData();
+      form.append('image', img);
+      form.append('state', encodeURIComponent(state))
 
-            const request = {
-                method: 'POST',
-                body: form
-            };
+      const request = {
+        method: 'POST',
+        body: form
+      };
 
-            fetch('http://localhost:8080/upload', request)
-                .then(response => console.log(response.text()))
-          }
+      fetch('http://localhost:8080/upload', request)
+        .then(response => console.log(response.text()))
     }
+  }
 
-    return (
-        <div>
-            <h1>hi</h1>
-            <input type="file" name="image" onChange={handleUpload} />
-        </div>
-    );
+  return (
+    <div>
+      <h1>hi</h1>
+      <input type="file" name="image" onChange={handleUpload} />
+    </div>
+  );
 }
 
 export default Upload;
